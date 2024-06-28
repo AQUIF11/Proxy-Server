c97f5e6909c1ee453041bc333c6c2c82e83b0bb8
Aquif R Mir <rezaaquif11@gmail.com>
2024-06-28 14:19:00 +0530

    Implementing response caching using apicache package

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@ const url = require("url");
 const express = require("express");
 const router = express.Router();
 const needle = require("needle");
+const apicache = require("apicache");
 
 // GET ENVIRONMENT VARIABLES.
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-router.get("/", async (req, res) => {
+// INITIALIZE CACHE
+let cache = apicache.middleware;
+
+// THIS ROUTE PROVIDES STORES AND PROVIDES THE CACHED RESPONSE FOR 2 MINUTES.
+router.get("/", cache("2 minutes"), async (req, res) => {
   try {
     // console.log(url.parse(req.url, true).query);
 
