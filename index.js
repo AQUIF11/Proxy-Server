425728494bf5cdeee8880297f106e85f2fd7e11e
Aquif R Mir <rezaaquif11@gmail.com>
2024-06-28 13:18:14 +0530

    Implement rate-limiting on the proxy-server using express-rate-limit package

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const indexRoutes = require("./routes/index");
 
 const app = express();
 
+// IMPLEMENT RATE-LIMITING
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // TIME DURATION IN MILLISECS(HERE 10 MINS)
+  max: 5, // MAX NUMBER OF REQUESTS IN THE SPECIFIED TIME DURATION
+});
+app.use(limiter);
+// ALSO SINCE THIS IS A PROXY-SERVER
+app.set("trust proxy", 1);
+
+// ROUTES
 app.use("/api", indexRoutes);
 
 // ENABLE CORS
